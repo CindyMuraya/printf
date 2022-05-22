957e265bb0647048ace9fb1baa7d3c1d14b1495c
Cindy Muraya <cmuraya12@gmail.com>
2022-05-22 14:33:44 -0700

    main.h

--- main.h
+++ main.h
@@ -15,21 +15,21 @@
 
 int _putchar(char c);
 int _printf(const char *format, ...);
-int print_percent(va_list list);
-int print_decimal(va_list list);
-int print_int(va_list list);
-int print_unsigned(va_list list);
+int printf_percent(va_list list);
+int printf_decimal(va_list list);
+int printf_int(va_list list);
+int printf_unsigned(va_list list);
 char *convert(unsigned long int num, int base, int lowercase);
-int print_binary(va_list list);
-int print_decimal(unsigned int x);
-int print_octal(unsigned int x);
-int print_hex(unsigned int x);
-int print_HEX(unsigned int x);
-int print_string(va_list list);
-int print_char(va_list list);
-int print_pointer(va_list list);
+int printf_binary(va_list list);
+int printf_decimal(unsigned int x);
+int printf_octal(unsigned int x);
+int printf_hex(unsigned int x);
+int printf_HEX(unsigned int x);
+int printf_string(va_list list);
+int printf_char(va_list list);
+int printf_pointer(va_list list);
 int get_flags(char s, list);
-int print_rev(va_list list);
-int print_rot13(va_list list);
+int printf_rev(va_list list);
+int printf_rot13(va_list list);
 
 #endif
