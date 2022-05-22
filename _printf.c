082e0a17a8ac4015ecc91ecc0775af64daecc6bf
Cindy Muraya <cmuraya12@gmail.com>
2022-05-22 14:30:13 -0700

    task 0

---
+++ _printf.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+/**
+ * _printf - a function that produces output according to a format
+ * @format: a character string composed of zero or more directives
+ * Return: the number of characters printed
+ * (excluding the null byte used to end output to strings)
+ * write output to stdout, the standard output stream
+ */
+
+int _printf(const char *format, ...);
+{
+	convert_match m[] = {
+		{"%c", print_char}, {"%s", print_string},
+		{"%%", print_percent},
+		{"%d", print_decimal}, {"%i", print_int},
+		{"%b", print_binary}, {"%u", print_decimal},
+		{"%o", print_octal}, {"%x", print_hex}, {"%X", print_HEX},
+		{"%S", print_string}, {"%p", print_pointer}
+	};
+
+	va_list list;
+	int i = 0, j, len = 0;
+
+	va_start(list, format);
+	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
+	{
+		return (-1);
+	}
+	while (format[i] != '\0')
+	{
+		j = 13;
+		while (j >= 0)
+		{
+			if (m[j].id[0] == format[i] && m[j].id[1] == format[i + 1])
+			{
+				len += m[j].f(list);
+				i = i + 2;
+			}
+			j--;
+		}
+		_putchar(format[i]);
+		len++;
+		i++;
+	}
+	va_end(list);
+	return (len);
+}
